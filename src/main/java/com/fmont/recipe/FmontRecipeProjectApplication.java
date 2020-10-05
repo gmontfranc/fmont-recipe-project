cea3fb7a1163e48497a4b90c78157d43f2c3334c
Montilla Francisco <montilla2808@gmail.com>
2020-10-05 18:56:30 +0200

    First commit of the project after initialization via spring initializer.

---
+++ src/main/java/com/fmont/recipe/FmontRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fmont.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmontRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmontRecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fmont/recipe/FmontRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmont.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmontRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
