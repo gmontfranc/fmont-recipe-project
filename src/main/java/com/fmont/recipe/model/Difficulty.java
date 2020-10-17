aa3b446b924f223b96fa9126d00b5a6a880dda0f
Montilla Francisco <montilla2808@gmail.com>
2020-10-17 14:07:19 +0200

    Adding difficulty for the recipes as enumeration and modifying default behaviour to enumtype STRING

---
+++ src/main/java/com/fmont/recipe/model/Difficulty.java
@@ -0,0 +1,6 @@
+package com.fmont.recipe.model;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/fmont/recipe/model/Recipe.java
+++ src/main/java/com/fmont/recipe/model/Recipe.java
@@ -18,16 +18,26 @@ public class Recipe {
     private String url;
     private String directions;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredientSet;
-    //todo add
-    //private Difficult difficult;
 
     @Lob
     private Byte[] image;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 
     public Set<Ingredient> getIngredientSet() {
         return ingredientSet;
@@ -36,6 +46,8 @@ public class Recipe {
     public void setIngredientSet(Set<Ingredient> ingredientSet) {
         this.ingredientSet = ingredientSet;
     }
+
+
 
     public Long getId() {
         return id;
