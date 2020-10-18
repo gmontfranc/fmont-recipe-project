7ef94b8705fd504fa07a1c7ac923c85e990ac071
Montilla Francisco <montilla2808@gmail.com>
2020-10-18 12:49:49 +0200

    Adding repositories for Category, Unit of measure and Recipe

---
+++ src/main/java/com/fmont/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.fmont.recipe.repositories;
+
+import com.fmont.recipe.model.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+}
